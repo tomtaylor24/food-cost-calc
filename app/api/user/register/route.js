c4184667e898815ee88709f5639883a497c36b32
Tomoaki Taruma <tottaruman@gmail.com>
2026-07-27 11:33:32 +0900

    登録のハッシュ化

--- app/api/user/register/route.js
+++ app/api/user/register/route.js
@@ -1,10 +1,16 @@
 import { NextResponse } from "next/server";
 import supabase from "@/app/utils/database";
+import bcrypt from "bcryptjs";
 
 export async function POST(request) {
   const reqBody = await request.json()
+  const hashedPassword = await bcrypt.hash(reqBody.password, 10)
   try{
-    const {error} = await supabase.from("users").insert(reqBody)
+    const {error} = await supabase.from("users")
+                                  .insert({
+                                    email: reqBody.email,
+                                    password_hash: hashedPassword
+                                  })
     if (error) throw new Error(error.message)
     return NextResponse.json({message: "ユーザー登録成功"})
 }catch(err){
