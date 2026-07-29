5810125bff6379ebc309fe5ebe5399f6fd8e9e57
Tomoaki Taruma <tottaruman@gmail.com>
2026-07-29 10:41:16 +0900

    食材登録、食材一覧取得のAPIを追加

---
+++ app/api/ingredients/route.js
@@ -0,0 +1,53 @@
+import { NextResponse } from "next/server";
+import supabase from "@/app/utils/database";
+import verifyToken from "@/app/utils/verifyToken";
+
+export async function POST(request) {
+  const reqBody = await request.json()
+  const payload = await verifyToken(request)
+
+  if (!payload) {
+    return NextResponse.json({ message: "トークンが有効ではありません" }, { status: 401 })
+  } else {
+    try {
+      const { error } = await supabase
+        .from("ingredients")
+        .insert({
+          user_id: payload.userId,
+          name: reqBody.name,
+          purchase_price: Number(reqBody.purchasePrice),
+          purchase_quantity: Number(reqBody.purchaseQuantity),
+          unit: reqBody.unit
+        })
+      if (error) throw new Error(error.message)
+      return NextResponse.json({ message: "食材登録成功" }, { status: 201 })
+    } catch (error) {
+      return NextResponse.json({ message: `食材登録失敗：${error}` }, { status: 500 })
+    }
+  }
+
+}
+
+
+export async function GET(request){
+    const payload = await verifyToken(request)
+    if(!payload){
+    return NextResponse.json({ message: "トークンが有効ではありません" }, { status: 401 })
+    }else{
+      try{
+        const {data, error} = await supabase
+        .from("ingredients")
+        .select()
+        .eq("user_id", payload.userId)
+        if(error) throw new Error(error.message)
+        return NextResponse.json({
+          message: "食材一覧の取得成功",
+          ingredients: data
+        },
+        {status: 200 }
+      )
+      }catch(error){
+        return NextResponse.json({message: `食材一覧の取得失敗:${error}`},{status: 500})
+      }
+    }
+}
